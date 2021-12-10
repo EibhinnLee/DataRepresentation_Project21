16019c7f06d0d7e56e0c986481d32e5875d3c639
Eibhinn Lee <g00226502@gmit.ie>
2021-12-10 00:46:22 +0000

    DataRep Project

---
+++ initdb.sql
@@ -0,0 +1,9 @@
+use datarepresentation;
+
+create table horses(
+    -> Racenumber int AUTO_INCREMENT PRIMARY KEY,
+    -> Horse varchar(250),
+    -> Jockey varchar(250),
+    -> Trainer varchar(250),
+    -> Age int
+    -> );
